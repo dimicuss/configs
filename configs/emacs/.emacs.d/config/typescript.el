8c6942ad64ab3a651c52b9418d3102b533bb2ca1
Dmitry <dimicuss@gmail.com>
2022-09-02 13:05:55 +0300

    Update typescript.el

--- configs/emacs/.emacs.d/config/typescript.el
+++ configs/emacs/.emacs.d/config/typescript.el
@@ -1,5 +1,6 @@
 (require 'use-package)
 
+(use-package web-mode :ensure t)
 (use-package company :ensure t)
 (use-package tide :ensure t)
 
@@ -7,10 +8,17 @@
   (interactive)
   (tide-setup)
   (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled idle-change))
   (eldoc-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (tide-hl-identifier-mode +1)
   (company-mode +1))
 
+(defun setup-tsx ()
+  (when (string-equal "tsx" (file-name-extension buffer-file-name))
+    (setup-tide-mode)))
+
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+
 (add-hook 'before-save-hook 'tide-format-before-save)
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
+(add-hook 'web-mode-hook #'setup-tsx)
